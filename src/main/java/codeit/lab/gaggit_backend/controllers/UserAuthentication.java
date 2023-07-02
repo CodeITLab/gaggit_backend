47cb8a347d955b11a25bdb3f7183ddd0ba18b259
kristijanfistrek <fistrekkristijan@gmail.com>
2023-07-02 16:13:27 +0200

    Add: Cross origin for authentication controller

--- src/main/java/codeit/lab/gaggit_backend/controllers/UserAuthentication.java
+++ src/main/java/codeit/lab/gaggit_backend/controllers/UserAuthentication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserAuthentication {
     @GetMapping(path = "/isUserLoggedIn")
     @ResponseBody
+    @CrossOrigin(origins = "http://127.0.0.1:5501")
     public ResponseEntity<User> Welcome() {
         return new ResponseEntity(new User(0, "Ivan", true),
                 HttpStatusCode.valueOf(200));
